7ae38804fdc74058ef05b8e29b78dae0568a1af5
bucky <buckyroberts@gmail.com>
2023-06-21 19:22:02 -0400

    Working on exchange UI

--- src/pages/Exchange/Styles.ts
+++ src/pages/Exchange/Styles.ts
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
 
+import {colors, fonts} from 'styles';
+
+export const AssetPairButton = styled.button`
+  background: transparent;
+  border: 1px solid rgb(207, 217, 222);
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: ${fonts.weight.semiBold};
+  padding: 8px 16px;
+  transition: all 0.1s;
+
+  &:hover {
+    background: ${colors.whiteHover};
+  }
+`;
+
 export const Container = styled.div`
-  padding: 8px 0 24px;
+  padding: 24px 16px;
+`;
+
+export const Tab = styled.div<{$isActive?: boolean}>`
+  background: ${({$isActive}) => ($isActive ? colors.palette.blue['400'] : 'transparent')};
+  border-radius: 4px;
+  color: ${({$isActive}) => ($isActive ? '#fff' : colors.primary)};
+  font-size: 13px;
+  font-weight: 500;
+  padding: 8px 16px;
+
+  &:hover {
+    color: ${({$isActive}) => ($isActive ? '#fff' : '#4458b8')};
+    cursor: pointer;
+  }
+`;
+
+export const TabContent = styled.div`
+  border-radius: 4px;
+  border: 1px solid ${colors.border};
+  padding: 16px;
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  margin-top: 16px;
 `;
--- src/pages/Exchange/index.tsx
+++ src/pages/Exchange/index.tsx
@@ -1,10 +1,37 @@
+import {useState} from 'react';
+
 import {SFC} from 'types';
 import * as S from './Styles';
 
+enum Tab {
+  buy = 'buy',
+  sell = 'sell',
+}
+
 const Exchange: SFC = ({className}) => {
+  const [activeTab, setActiveTab] = useState(Tab.buy);
+
+  const renderTabContent = () => {
+    const tabContent = {
+      [Tab.buy]: <h4>Buy</h4>,
+      [Tab.sell]: <h4>Sell</h4>,
+    };
+
+    return <S.TabContent>{tabContent[activeTab]}</S.TabContent>;
+  };
+
   return (
     <S.Container className={className}>
-      <div>Exchange</div>
+      <S.AssetPairButton>VTX-TNB</S.AssetPairButton>
+      <S.Tabs>
+        <S.Tab $isActive={activeTab === Tab.buy} onClick={() => setActiveTab(Tab.buy)}>
+          Buy
+        </S.Tab>
+        <S.Tab $isActive={activeTab === Tab.sell} onClick={() => setActiveTab(Tab.sell)}>
+          Sell
+        </S.Tab>
+      </S.Tabs>
+      {renderTabContent()}
     </S.Container>
   );
 };
