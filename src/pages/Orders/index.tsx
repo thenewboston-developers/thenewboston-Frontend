c9235eab877a2e668aa7c9a2c099d6b2d84a94c7
bucky <buckyroberts@gmail.com>
2023-06-27 13:25:45 -0400

    Optimize orders page logic

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,7 +1,10 @@
-import {useMemo} from 'react';
+import {useCallback, useMemo} from 'react';
 import {useSelector} from 'react-redux';
 import orderBy from 'lodash/orderBy';
+import {mdiDotsVertical} from '@mdi/js';
 
+import DropdownMenu from 'components/DropdownMenu';
+import {FillStatus} from 'enums';
 import {getCores, getOrders, getSelf} from 'selectors/state';
 import {SFC} from 'types';
 import * as S from './Styles';
@@ -16,9 +19,33 @@ const Orders: SFC = ({className}) => {
     return orderedOrders.filter((order) => order.owner === self.id);
   }, [orders, self.id]);
 
-  const renderRows = () => {
-    return filteredOrders.map(
-      ({
+  const getCurrencyTicker = useCallback((coreId: number) => cores[coreId]?.ticker || '-', [cores]);
+
+  const renderDropdownMenu = useCallback((fill_status: FillStatus) => {
+    const menuOptions = [
+      {
+        label: 'View Trades',
+        onClick: () => {
+          console.log('View Trades');
+        },
+      },
+    ];
+
+    if ([FillStatus.OPEN, FillStatus.PARTIALLY_FILLED].includes(fill_status)) {
+      menuOptions.unshift({
+        label: 'Cancel Order',
+        onClick: () => {
+          console.log('Cancel Order');
+        },
+      });
+    }
+
+    return <DropdownMenu icon={mdiDotsVertical} options={menuOptions} />;
+  }, []);
+
+  const renderRows = useCallback(() => {
+    return filteredOrders.map((order) => {
+      const {
         created_date,
         id,
         filled_amount,
@@ -28,29 +55,30 @@ const Orders: SFC = ({className}) => {
         price,
         primary_currency,
         secondary_currency,
-      }) => {
-        const primaryCurrencyTicker = cores[primary_currency]?.ticker || '-';
-        const secondaryCurrencyTicker = cores[secondary_currency]?.ticker || '-';
+      } = order;
 
-        return (
-          <tr key={id}>
-            <td>{created_date}</td>
-            <td>{order_type}</td>
-            <td>
-              {quantity} {primaryCurrencyTicker}
-            </td>
-            <td>
-              {price} {secondaryCurrencyTicker}
-            </td>
-            <td>
-              {filled_amount} {primaryCurrencyTicker}
-            </td>
-            <td>{fill_status}</td>
-          </tr>
-        );
-      },
-    );
-  };
+      const primaryCurrencyTicker = getCurrencyTicker(primary_currency);
+      const secondaryCurrencyTicker = getCurrencyTicker(secondary_currency);
+
+      return (
+        <tr key={id}>
+          <td>{created_date}</td>
+          <td>{order_type}</td>
+          <td>
+            {quantity} {primaryCurrencyTicker}
+          </td>
+          <td>
+            {price} {secondaryCurrencyTicker}
+          </td>
+          <td>
+            {filled_amount} {primaryCurrencyTicker}
+          </td>
+          <td>{fill_status}</td>
+          <td>{renderDropdownMenu(fill_status)}</td>
+        </tr>
+      );
+    });
+  }, [filteredOrders, getCurrencyTicker, renderDropdownMenu]);
 
   return (
     <S.Container className={className}>
@@ -64,6 +92,7 @@ const Orders: SFC = ({className}) => {
             <th>Price</th>
             <th>Filled Amount</th>
             <th>Fill Status</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>{renderRows()}</tbody>
