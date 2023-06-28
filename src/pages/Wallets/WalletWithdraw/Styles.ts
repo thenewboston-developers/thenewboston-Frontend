280f095ce32f411fa2724104652c56fce20ac634
Bucky Roberts <buckyroberts@gmail.com>
2023-06-28 15:00:02 -0400

    Withdraw UI (#31)

--- src/pages/Wallets/WalletWithdraw/Styles.ts
+++ src/pages/Wallets/WalletWithdraw/Styles.ts
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 
 import {colors} from 'styles';
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+// TODO: Standardize
+export const Panel = styled.div`
   border-radius: 4px;
   border: 1px solid ${colors.border};
+  margin-top: 16px;
   padding: 16px;
 `;
--- src/pages/Wallets/WalletWithdraw/index.tsx
+++ src/pages/Wallets/WalletWithdraw/index.tsx
@@ -1,8 +1,66 @@
+import {useMemo} from 'react';
+import {Form, Formik} from 'formik';
+
+import Button, {ButtonType} from 'components/Button';
+import {Input} from 'components/FormElements';
 import {SFC} from 'types';
+import {displayErrorToast} from 'utils/toast';
+import yup, {accountNumberSchema} from 'utils/yup';
 import * as S from './Styles';
 
 const WalletWithdraw: SFC = ({className}) => {
-  return <S.Container className={className}>withdraw</S.Container>;
+  const initialValues = {
+    accountNumber: '',
+    amount: '',
+  };
+
+  type FormValues = typeof initialValues;
+
+  const handleSubmit = async (values: FormValues): Promise<void> => {
+    try {
+      const requestData = {
+        account_number: values.accountNumber,
+        amount: parseInt(values.amount, 10),
+      };
+      console.log(requestData);
+    } catch (error) {
+      console.error(error);
+      displayErrorToast('Error withdrawing funds');
+    }
+  };
+
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      accountNumber: accountNumberSchema.required(),
+      amount: yup.number().integer('Amount must be an integer').moreThan(1).required(),
+    });
+  }, []);
+
+  return (
+    <S.Container className={className}>
+      <S.Panel>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+          {({dirty, errors, isSubmitting, isValid, touched}) => (
+            <Form>
+              <Input errors={errors} label="Amount" name="amount" touched={touched} type="number" />
+              <Input errors={errors} label="Account Number" name="accountNumber" touched={touched} />
+              <Button
+                dirty={dirty}
+                disabled={isSubmitting}
+                isSubmitting={isSubmitting}
+                isValid={isValid}
+                text="Withdraw"
+                type={ButtonType.submit}
+              />
+            </Form>
+          )}
+        </Formik>
+      </S.Panel>
+      <S.Panel>
+        <h4>Withdraws</h4>
+      </S.Panel>
+    </S.Container>
+  );
 };
 
 export default WalletWithdraw;
