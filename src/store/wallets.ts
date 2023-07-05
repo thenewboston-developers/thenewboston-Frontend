433d25a52dc314a7cfc525d57c5aabf77576d35a
bucky <buckyroberts@gmail.com>
2023-07-05 14:21:36 -0400

    40 - Handle modified date after wallet update

--- src/store/wallets.ts
+++ src/store/wallets.ts
@@ -10,8 +10,8 @@ const wallets = createSlice({
   name: WALLETS,
   reducers: {
     setWallet: (state: Wallets, {payload}: PayloadAction<Wallet>) => {
-      const {id} = payload;
-      state[id] = payload;
+      const {id, modified_date} = payload;
+      if (!state[id] || new Date(modified_date) > new Date(state[id].modified_date)) state[id] = payload;
     },
     setWallets: (state: Wallets, {payload}: PayloadAction<Wallet[]>) => {
       return payload.reduce((acc: Wallets, obj) => ({...acc, [obj.id]: obj}), {});
--- src/types/wallets.ts
+++ src/types/wallets.ts
@@ -4,9 +4,11 @@ import {Dict} from 'types/generic';
 export interface Wallet {
   balance: number;
   core: Core;
+  created_date: string;
   deposit_account_number: string;
   deposit_balance: number;
   id: number;
+  modified_date: string;
   owner: number;
 }
 
