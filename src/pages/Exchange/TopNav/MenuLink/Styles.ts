5fc324fc04c351326594be66d2b492089278128f
bucky <buckyroberts@gmail.com>
2023-07-11 18:15:41 -0400

    Adding isActive logic

--- src/pages/Exchange/TopNav/MenuLink/Styles.ts
+++ src/pages/Exchange/TopNav/MenuLink/Styles.ts
@@ -1,9 +1,12 @@
 import {Link as ULink} from 'react-router-dom';
 import styled from 'styled-components';
 
+import {fonts} from 'styles';
+
 export const Container = styled(ULink)<{$isActive: boolean}>`
+  background: ${({$isActive}) => ($isActive ? 'rgba(144, 157, 171, 0.2)' : 'transparent')};
   color: #fff;
-  font-weight: 600;
+  font-weight: ${fonts.weight.semiBold};
   padding: 12px 8px;
 
   &:hover {
