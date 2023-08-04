e0d4b44193d7c037836ade1388b8ca23894eee68
bucky <buckyroberts@gmail.com>
2023-08-04 14:51:05 -0400

    Empty states completed

--- src/pages/Cores/index.tsx
+++ src/pages/Cores/index.tsx
@@ -1,7 +1,10 @@
+import {useMemo} from 'react';
 import {useSelector} from 'react-redux';
 import orderBy from 'lodash/orderBy';
 
 import Button from 'components/Button';
+import EmptyText from 'components/EmptyText';
+import SectionHeading from 'components/SectionHeading';
 import {useToggle} from 'hooks';
 import CoreModal from 'modals/CoreModal';
 import {getCores} from 'selectors/state';
@@ -13,18 +16,29 @@ const Cores: SFC = ({className}) => {
   const [coreModalIsOpen, toggleCoreModal] = useToggle(false);
   const cores = useSelector(getCores);
 
-  const renderCoreCards = () => {
-    const orderedCores = orderBy(Object.values(cores), ['ticker']);
-    return orderedCores.map((core) => <CoreCard core={core} key={core.id} />);
+  const coresList = useMemo(() => {
+    return orderBy(Object.values(cores), ['ticker']);
+  }, [cores]);
+
+  const renderButton = () => {
+    return <Button onClick={toggleCoreModal} text="Add Core" />;
+  };
+
+  const renderContent = () => {
+    if (!!coresList.length) return renderCores();
+    return <EmptyText>No cores to display.</EmptyText>;
+  };
+
+  const renderCores = () => {
+    const coreCards = coresList.map((core) => <CoreCard core={core} key={core.id} />);
+    return <S.CardsContainer>{coreCards}</S.CardsContainer>;
   };
 
   return (
     <>
       <S.Container className={className}>
-        <S.Toolbar>
-          <Button onClick={toggleCoreModal} text="Add Core" />
-        </S.Toolbar>
-        <S.CardsContainer>{renderCoreCards()}</S.CardsContainer>
+        <SectionHeading heading="Cores" rightContent={renderButton()} />
+        {renderContent()}
       </S.Container>
       {coreModalIsOpen ? <CoreModal close={toggleCoreModal} /> : null}
     </>
