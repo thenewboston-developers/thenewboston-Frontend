dcdf46c2598a817aef6c2f118139a12f2e060f9c
Bucky Roberts <buckyroberts@gmail.com>
2023-11-07 19:40:57 -0500

    134 - Make profile responsive (#136)

--- src/pages/Profile/Layout/Styles.ts
+++ src/pages/Profile/Layout/Styles.ts
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
+import {breakpoints} from 'styles';
+
 export const Container = styled.div`
   display: grid;
   gap: 32px;
-  grid-template-columns: 260px 1fr;
+  grid-template-columns: 1fr;
   padding: 24px 32px;
+
+  @media (min-width: ${breakpoints.tablet}) {
+    grid-template-columns: 260px 1fr;
+  }
 `;
 
 export const OutletContainer = styled.div`
