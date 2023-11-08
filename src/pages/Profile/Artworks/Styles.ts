fbf9a8c90792af76c780a079cbe46d4483e8ee6b
bucky <buckyroberts@gmail.com>
2023-11-07 19:39:54 -0500

    134 - Make profile responsive

--- src/pages/Profile/Artworks/Styles.ts
+++ src/pages/Profile/Artworks/Styles.ts
@@ -5,15 +5,23 @@ import {breakpoints} from 'styles';
 export const ArtworkCards = styled.div`
   display: grid;
   gap: 12px;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: 1fr;
   margin-top: 16px;
 
-  @media (max-width: ${breakpoints.mobile}) {
+  @media (min-width: ${breakpoints.mobile}) {
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (max-width: ${breakpoints.mini}) {
-    grid-template-columns: auto;
+  @media (min-width: ${breakpoints.desktop}) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: ${breakpoints.largeDesktop}) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media (min-width: ${breakpoints.xlDesktop}) {
+    grid-template-columns: repeat(5, 1fr);
   }
 `;
 
--- src/pages/Profile/Layout/UserDetails/Styles.ts
+++ src/pages/Profile/Layout/UserDetails/Styles.ts
@@ -1,26 +1,44 @@
 import styled from 'styled-components';
 
 import UButton from 'components/Button';
+import {breakpoints} from 'styles';
 
 export const Button = styled(UButton)`
   margin-top: 16px;
   width: 100%;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: ${breakpoints.tablet}) {
+    display: block;
+  }
+`;
 
 export const Img = styled.img`
-  border-radius: 50%;
   height: 100%;
   object-fit: cover;
-  position: absolute;
   width: 100%;
+
+  @media (min-width: ${breakpoints.tablet}) {
+    position: absolute;
+  }
 `;
 
 export const ImgWrapper = styled.div`
-  padding-bottom: 100%;
-  position: relative;
-  width: 240px;
+  border-radius: 50%;
+  height: 160px;
+  overflow: hidden;
+  width: 160px;
+
+  @media (min-width: ${breakpoints.tablet}) {
+    padding-bottom: 100%;
+    position: relative;
+    width: 260px;
+  }
 `;
 
 export const Username = styled.h1`
