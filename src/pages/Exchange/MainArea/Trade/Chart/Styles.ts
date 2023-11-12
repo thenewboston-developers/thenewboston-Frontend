0be4865dfe6cccd36eaa99ddc4e900fe6681bcdb
Bucky Roberts <buckyroberts@gmail.com>
2023-11-12 15:29:24 -0500

    143 - Order book styling fix (#144)
    * Order book styling
    
    * Responsive chart

--- src/pages/Exchange/MainArea/Trade/Chart/Styles.ts
+++ src/pages/Exchange/MainArea/Trade/Chart/Styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  overflow: hidden;
+`;
 
 export const LastPrice = styled.div`
   display: flex;
