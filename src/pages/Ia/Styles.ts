ac2dc5627c614db47a7cd49cdaf83a9892596e15
bucky <buckyroberts@gmail.com>
2024-01-11 21:11:20 -0500

    Ia basic navigation and layout

---
+++ src/pages/Ia/Styles.ts
@@ -0,0 +1,46 @@
+import {Form as UForm} from 'formik';
+import styled from 'styled-components';
+
+import UButton from 'components/Button';
+import {InlineInput as UInlineInput} from 'components/FormElements';
+import {colors} from 'styles';
+
+export const Bottom = styled.div`
+  padding: 16px 12px;
+`;
+
+export const Button = styled(UButton)`
+  display: none;
+`;
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 240px 1fr;
+  height: 100vh;
+`;
+
+export const Form = styled(UForm)`
+  align-items: center;
+  display: flex;
+`;
+
+export const LeftMenu = styled.div`
+  border-right: 1px solid ${colors.border};
+  overflow-y: auto;
+`;
+
+export const MessagesContainer = styled.div`
+  border-bottom: 1px solid ${colors.border};
+  overflow-y: auto;
+`;
+
+export const Right = styled.div`
+  display: grid;
+  grid-template-rows: auto min-content;
+  height: 100vh;
+`;
+
+export const TextInput = styled(UInlineInput)`
+  flex: 1;
+  margin-left: 12px;
+`;
---
+++ src/pages/Ia/index.tsx
@@ -0,0 +1,88 @@
+import {useSelector} from 'react-redux';
+import {Formik, FormikHelpers} from 'formik';
+
+import Avatar from 'components/Avatar';
+import {ButtonType} from 'components/Button';
+import {getSelf} from 'selectors/state';
+import {SFC} from 'types';
+import {displayErrorToast} from 'utils/toast';
+import yup from 'utils/yup';
+import * as S from './Styles';
+import {useMemo} from 'react';
+
+const Ia: SFC = ({className}) => {
+  const self = useSelector(getSelf);
+
+  const initialValues = {
+    text: '',
+  };
+
+  type FormValues = typeof initialValues;
+
+  const handleSubmit = async (values: FormValues, {resetForm}: FormikHelpers<FormValues>): Promise<void> => {
+    try {
+      console.log(values);
+      resetForm();
+    } catch (error) {
+      console.error(error);
+      displayErrorToast('Error submitting the message');
+    }
+  };
+
+  const renderBottom = () => {
+    return (
+      <S.Bottom>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validateOnMount={false}
+          validationSchema={validationSchema}
+        >
+          {({dirty, errors, isSubmitting, isValid, touched}) => (
+            <S.Form>
+              <Avatar src={self.avatar} />
+              <S.TextInput errors={errors} name="text" placeholder="Send a message..." touched={touched} />
+              <S.Button
+                dirty={dirty}
+                disabled={isSubmitting}
+                isSubmitting={isSubmitting}
+                isValid={isValid}
+                text=""
+                type={ButtonType.submit}
+              />
+            </S.Form>
+          )}
+        </Formik>
+      </S.Bottom>
+    );
+  };
+
+  const renderMessagesContainer = () => {
+    return (
+      <S.MessagesContainer>
+        <h1>message 1</h1>
+        <h1>message 2</h1>
+      </S.MessagesContainer>
+    );
+  };
+
+  const validationSchema = useMemo(() => {
+    return yup.object().shape({
+      text: yup.string().required(),
+    });
+  }, []);
+
+  return (
+    <S.Container className={className}>
+      <S.LeftMenu>
+        <h1>convo 1</h1>
+      </S.LeftMenu>
+      <S.Right>
+        {renderMessagesContainer()}
+        {renderBottom()}
+      </S.Right>
+    </S.Container>
+  );
+};
+
+export default Ia;
