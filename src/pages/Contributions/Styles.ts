b665377ab71240df2371ca03d71e4fa78eb7552b
bucky <buckyroberts@gmail.com>
2024-01-15 16:20:53 -0500

    Starting on contributions table

--- src/pages/Contributions/Styles.ts
+++ src/pages/Contributions/Styles.ts
@@ -3,3 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div`
   padding: 24px 32px;
 `;
+
+export const Table = styled.table`
+  margin-top: 12px;
+  width: 100%;
+`;
+
+export const Tr = styled.tr``;
+
+export const Td = styled.td``;
--- src/pages/Contributions/index.tsx
+++ src/pages/Contributions/index.tsx
@@ -3,9 +3,41 @@ import {SFC} from 'types';
 import * as S from './Styles';
 
 const Contributions: SFC = ({className}) => {
+  const renderTable = () => {
+    return (
+      <S.Table>
+        <thead>
+          <S.Tr>
+            <th>Date</th>
+            <th>User</th>
+            <th>GitHub User</th>
+            <th>Repo</th>
+            <th>Issue</th>
+            <th>PR</th>
+            <th>Reward Amount</th>
+          </S.Tr>
+        </thead>
+        <tbody>
+          {[1, 2].map((number) => (
+            <S.Tr key={number}>
+              <S.Td>November 12, 2023 at 3:00:06 PM EST</S.Td>
+              <S.Td>BUCKY</S.Td>
+              <S.Td>buckyroberts</S.Td>
+              <S.Td>TNB-BACKEND</S.Td>
+              <S.Td>ISSUE 1</S.Td>
+              <S.Td>PR 2</S.Td>
+              <S.Td>2,600 TNB</S.Td>
+            </S.Tr>
+          ))}
+        </tbody>
+      </S.Table>
+    );
+  };
+
   return (
     <S.Container className={className}>
       <SectionHeading heading="Contributions" />
+      {renderTable()}
     </S.Container>
   );
 };
