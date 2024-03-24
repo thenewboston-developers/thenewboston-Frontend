13754540f81f5fbcb0b7585ab65c6a46b4a7c051
Bucky Roberts <buckyroberts@gmail.com>
2024-03-24 17:00:04 -0400

    155 - Make post content a textarea (#165)
    * 155 - Make post content a textarea
    
    * Remove dead code

--- src/modals/PostModal/Styles.ts
+++ src/modals/PostModal/Styles.ts
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
 
-import {Input as UInput} from 'components/FormElements';
 import UModal from 'components/Modal';
 
 export const Bumper = styled.div`
   margin-bottom: 24px;
-`;
-
-export const Input = styled(UInput)`
-  width: 100%;
 `;
 
 export const Modal = styled(UModal)`
