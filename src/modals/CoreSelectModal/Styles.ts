ff6070425a8075eb6d816274dd4e8cbcf67eac5d
Abdurrehman <abdurrehman.user.95@gmail.com>
2024-03-29 01:18:19 +0500

    style: modal also made responsive

--- src/modals/CoreSelectModal/Styles.ts
+++ src/modals/CoreSelectModal/Styles.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
 import UModal from 'components/Modal';
+import {breakpoints} from 'styles';
 
 export const Modal = styled(UModal)`
   display: flex;
   flex-direction: column;
   max-width: 500px;
   min-width: 480px;
+
+  @media (max-width: ${breakpoints.mini}) {
+    min-width: 90%;
+    max-width: 100%;
+  }
 `;
 
 export const RadioCardContainer = styled.div`
