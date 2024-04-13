33543d547fb1513da4cddfb6ec736b7e0c0d1021
Abdurrehman <abdurrehman.user.95@gmail.com>
2024-04-14 02:28:08 +0500

    fix: validation added for contributions list

--- src/pages/Contributions/index.tsx
+++ src/pages/Contributions/index.tsx
@@ -2,6 +2,7 @@ import {useEffect, useMemo} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import orderBy from 'lodash/orderBy';
 
+import EmptyText from 'components/EmptyText';
 import TotalContributionsChart from './TotalContributionsChart';
 import LatestContributions from './LatestContributions';
 import TopContributors from './TopContributors';
@@ -26,6 +27,14 @@ const Contributions: SFC<ContributionsProps> = ({className}) => {
   const contributionList = useMemo(() => {
     return orderBy(Object.values(contributions), ['created_date'], ['asc']);
   }, [contributions]);
+
+  if (!(contributionList && contributionList.length)) {
+    return (
+      <S.Container className={className}>
+        <EmptyText>No Contributions found</EmptyText>
+      </S.Container>
+    );
+  }
 
   return (
     <S.Container className={className}>
