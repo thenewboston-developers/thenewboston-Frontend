dac4ee3a007267fa294abe313f0924c70d60cb82
Bucky Roberts <buckyroberts@gmail.com>
2024-04-23 21:26:27 -0400

    218 - Update artwork card styling (#226)

--- src/components/ArtworkCard/Styles.ts
+++ src/components/ArtworkCard/Styles.ts
@@ -3,16 +3,17 @@ import styled from 'styled-components';
 import UDropdownMenu from 'components/DropdownMenu';
 import ULine from 'components/Line';
 import UThumbnail from 'components/Thumbnail';
-import {colors} from 'styles';
+import {colors, fonts} from 'styles';
 
 export const Bottom = styled.div`
   display: flex;
-  padding: 12px 16px;
+  flex: 1;
+  padding: 12px 16px 16px;
 `;
 
 export const Container = styled.div`
   background: #fff;
-  border-radius: 4px;
+  border-radius: 14px;
   border: 1px solid ${colors.border};
   box-shadow: 0 3px 6px rgb(140 149 159 / 15%);
   overflow: hidden;
@@ -25,6 +26,11 @@ export const Description = styled.div`
   margin-top: 4px;
 `;
 
+export const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const DropdownMenu = styled(UDropdownMenu)`
   margin-right: -8px;
 `;
@@ -34,14 +40,22 @@ export const Line = styled(ULine)`
 `;
 
 export const Name = styled.div`
-  font-size: 20px;
-  font-weight: 600;
+  font-size: 16px;
+  font-weight: ${fonts.weight.semiBold};
 `;
 
-export const Text = styled.div`
-  flex: 1;
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Thumbnail = styled(UThumbnail)`
   border-radius: 0;
+`;
+
+export const Top = styled.div`
+  border-bottom: 1px solid #f1f1f1;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 16px 12px;
 `;
