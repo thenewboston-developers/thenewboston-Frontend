c3c74aa4c87e694e0324d0fad102ee887a248f23
bswnth48 <69203760+bswnth48@users.noreply.github.com>
2024-04-30 23:59:07 +0530

    217 fixed strange scroll behavior

--- src/components/InfiniteScroll/Styles.ts
+++ src/components/InfiniteScroll/Styles.ts
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
 
 export const InfiniteScrollContainer = styled.div`
-  overflow: hidden;
+  overflow-y: auto;
 
-  .infinite-scroll-component {
-    overflow-y: scroll;
-    scrollbar-width: none; // For Firefox
-
-    &::-webkit-scrollbar {
-      display: none; // For Chrome, Safari, and Opera
-    }
+  &::-webkit-scrollbar {
+    width: 0;
   }
+  &::-webkit-scrollbar-thumb {
+    background: transparent;
+  }
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  width: 100%;
+  height: 100%;
 `;
 
 export const LoaderContainer = styled.div`
