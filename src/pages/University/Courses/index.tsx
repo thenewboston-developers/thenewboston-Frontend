85a9777ed192bbcd2eca63731558ed328e76c60e
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2024-05-06 16:37:12 +0530

    297 - Remove University search feature (#297)

--- src/pages/University/Courses/index.tsx
+++ src/pages/University/Courses/index.tsx
@@ -2,12 +2,10 @@ import {useDispatch, useSelector} from 'react-redux';
 import {useEffect, useMemo, useState} from 'react';
 
 import {AppDispatch, SFC} from 'types';
-import {colors} from 'styles';
 import {Col, Row} from 'styles/components/GridStyle';
 import {displayErrorToast} from 'utils/toasts';
 import {getCourses as _getCourses, resetCourses as _resetCourses} from 'dispatchers/courses';
 import {getCourses as _getCoursesState, getSelf} from 'selectors/state';
-import {mdiMagnify} from '@mdi/js';
 import {PublicationStatus} from 'enums';
 import {UniversityHeader} from './Header';
 import {useToggle} from 'hooks';
@@ -19,7 +17,6 @@ import InfiniteScroll from 'components/InfiniteScroll';
 import LeavesEmptyState from 'assets/leaves-empty-state.png';
 import Loader from 'components/Loader';
 import Toolbar from 'pages/University/Toolbar';
-import Tooltip from 'components/Tooltip';
 
 import * as S from './Styles';
 
@@ -109,12 +106,6 @@ const Courses: SFC<CoursesProps> = ({className, selfCourses = false}) => {
     return (
       <S.SectionSubHeading>
         <h2>{subHeading}</h2>
-        <Tooltip text="Feature coming soon...">
-          <S.SearchContainer className="disabled">
-            <S.Input placeholder="Search" />
-            <S.Icon color={colors.gray} path={mdiMagnify} size={'20px'} />
-          </S.SearchContainer>
-        </Tooltip>
       </S.SectionSubHeading>
     );
   };
