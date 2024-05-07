4c349ff60f02530cfbd0b0e155e98e53916ece86
Esayas Nigussie <esayasnigusie2@gmail.com>
2024-05-07 06:33:53 +0300

    change bg color, border, shadow (#308)
    * change bg color, border, shadow
    
    * Cleaning
    
    ---------
    
    Co-authored-by: bucky <buckyroberts@gmail.com>

--- src/components/Tab/Styles.ts
+++ src/components/Tab/Styles.ts
@@ -2,16 +2,21 @@ import styled from 'styled-components';
 
 import {colors} from 'styles';
 
+const HEIGHT = 34;
+const BORDER_RADIUS = HEIGHT / 2;
+
 export const Container = styled.div<{$isActive?: boolean}>`
+  align-items: center;
   background: ${({$isActive}) => ($isActive ? colors.white : 'transparent')};
-  border-radius: 100px;
+  border-radius: ${`${BORDER_RADIUS}px`};
+  box-shadow: ${({$isActive}) => ($isActive ? '0 2px 4px rgb(0 0 0 / 8%)' : 'none')};
   color: ${({$isActive}) => ($isActive ? colors.black : colors.primary)};
+  display: flex;
   font-size: 13px;
   font-weight: ${({$isActive}) => ($isActive ? 600 : 400)};
+  gap: 10px;
+  height: ${`${HEIGHT}px`};
   padding: 8px 16px;
-  display: flex;
-  align-items: center;
-  gap: 8px;
 
   &:hover {
     color: ${({$isActive}) => ($isActive ? colors.black : '#4458b8')};
--- src/components/Tabs/Styles.ts
+++ src/components/Tabs/Styles.ts
@@ -1,12 +1,19 @@
 import styled from 'styled-components';
+
 import {breakpoints} from 'styles';
 
+const HEIGHT = 48;
+const BORDER_RADIUS = HEIGHT / 2;
+
 export const Container = styled.div`
+  background: #ececee;
+  border-radius: ${`${BORDER_RADIUS}px`};
+  border: 1px solid #dfdfdf;
   display: flex;
-  border-radius: 100px;
-  background: #dfdfdf;
-  padding: 6px;
+  height: ${`${HEIGHT}px`};
   overflow-x: auto;
+  padding: 6px;
+
   @media (min-width: ${breakpoints.mobile}) {
     width: fit-content;
   }
