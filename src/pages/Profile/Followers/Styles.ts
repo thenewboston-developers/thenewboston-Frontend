9ab970d39a0b7819f7d314c579a19e24437e0ec4
Nija Kukadiya <nijakukadiya@Nijas-MacBook-Pro.local>
2024-05-10 17:58:36 +0530

    288 - Redesign followers and following sections (#288)

--- src/pages/Profile/Followers/Styles.ts
+++ src/pages/Profile/Followers/Styles.ts
@@ -1,10 +1,133 @@
 import styled from 'styled-components';
+import {colors, fonts} from 'styles';
 
 export const FollowerCards = styled.div`
   display: grid;
-  gap: 12px;
-  grid-template-columns: 1fr;
-  margin-top: 16px;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #fff;
+  border-radius: 14px;
+  box-shadow: 0 2px 4px rgb(0 0 0 / 8%);
+  margin-top: 16px;
+`;
+export const Rank = styled.div`
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+`;
+
+export const UserLabelContainer = styled.div``;
+
+export const RewardAmountContainer = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 14px;
+  font-weight: ${fonts.weight.semiBold};
+  justify-content: start;
+  width: 100%;
+`;
+export const UserLabel = styled.div`
+  flex-grow: 1;
+`;
+export const StatusFollow = styled.div`
+  font-size: 12px;
+  text-transform: capitalize;
+  font-weight: ${fonts.weight.semiBold};
+  color: ${colors.palette.blue['500']};
+`;
+
+export const StatusFollowing = styled.div`
+  font-size: 12px;
+  text-transform: capitalize;
+  font-weight: ${fonts.weight.semiBold};
+  color: ${colors.palette.red['500']};
+`;
+
+export const TableGrid = styled.div`
+  width: 100%;
+  grid-template-columns: 1fr 16fr 2fr;
+  display: grid;
+`;
+export const UserName = styled.div`
+  font-size: 16px;
+  text-transform: capitalize;
+`;
+
+export const Status = styled.div`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+`;
+
+export const FollowerUser = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+export const ContributorContainer = styled.div`
+  align-items: center;
+  border-bottom: 1px solid #f1f1f1;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 20px;
+  padding: 8px 0;
+
+  &:first-child {
+    padding-top: 20px;
+  }
+
+  &:last-child {
+    border-bottom: none;
+    padding-bottom: 20px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 20px;
+  padding-top: 23px;
+`;
+
+export const Title = styled.div`
+  font-size: 20px;
+  color: ${colors.gray};
+  font-weight: ${fonts.weight.semiBold};
+
+  & span {
+    color: ${colors.black};
+  }
+`;
+
+export const Search = styled.div`
+  position: relative;
+  display: flex;
+  height: 46px;
+  width: 300px;
+  background-color: #f4f4f4;
+  border-radius: 100px;
+
+  & svg {
+    height: 20px;
+    width: 20px;
+    position: absolute;
+    left: 14px;
+    top: 50%;
+    transform: translateY(-50%);
+    color: ${colors.gray};
+  }
+
+  & input {
+    width: 100%;
+    padding-left: 40px;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    font-size: 14px;
+    font-weight: ${fonts.weight.regular};
+    color: ${colors.gray};
+    border-radius: 100px;
+  }
+`;
--- src/pages/Profile/Following/index.tsx
+++ src/pages/Profile/Following/index.tsx
@@ -5,10 +5,33 @@ import orderBy from 'lodash/orderBy';
 
 import {getFollowers} from 'api/followers';
 import EmptyText from 'components/EmptyText';
-import UserLabel from 'components/UserLabel';
 import {AppDispatch, FollowerReadSerializer, SFC} from 'types';
 import {displayErrorToast} from 'utils/toasts';
 import * as S from './Styles';
+import Avatar from 'components/Avatar';
+import DefaultAvatar from 'assets/default-avatar.png';
+import {mdiAccountPlusOutline, mdiCheckCircleOutline, mdiMagnify} from '@mdi/js';
+import Icon from '@mdi/react';
+import {colors} from 'styles';
+
+const mockFollowers = [
+  {
+    follower: {
+      avatar: DefaultAvatar,
+      id: 1,
+      status: 'true',
+      username: 'ABC2',
+    },
+  },
+  {
+    follower: {
+      avatar: DefaultAvatar,
+      id: 2,
+      status: 'false',
+      username: 'ABC',
+    },
+  },
+];
 
 const Following: SFC = ({className}) => {
   const [followers, setFollowers] = useState<FollowerReadSerializer[]>([]);
@@ -38,23 +61,56 @@ const Following: SFC = ({className}) => {
   }, [followers]);
 
   const renderContent = () => {
-    if (!!followers.length) return renderFollowerCards();
+    if (!!mockFollowers.length) return renderFollowerCards();
     return <EmptyText>No followers to display.</EmptyText>;
   };
 
   const renderFollowerCards = () => {
-    const _followers = followerList.map(({following}) => (
-      <UserLabel
-        avatar={following.avatar}
-        description={`User ID: ${following.id}`}
-        id={following.id}
-        username={following.username}
-      />
+    const _followers = mockFollowers.map(({follower}) => (
+      <S.ContributorContainer key={follower.id}>
+        <S.TableGrid>
+          <S.Rank>{follower.id}</S.Rank>
+          <S.UserLabelContainer>
+            <S.FollowerCards>
+              <S.FollowerUser>
+                <Avatar src={follower.avatar} />
+                <S.UserName>{follower.username}</S.UserName>
+              </S.FollowerUser>
+            </S.FollowerCards>
+          </S.UserLabelContainer>
+          <S.RewardAmountContainer>
+            {follower.status === 'true' ? (
+              <S.Status>
+                <Icon path={mdiAccountPlusOutline} size={1} color={`${colors.palette.blue['500']}`} />
+                <S.StatusFollow>Follow</S.StatusFollow>
+              </S.Status>
+            ) : (
+              <S.Status>
+                <Icon path={mdiCheckCircleOutline} size={1} color={`${colors.palette.red['500']}`} />
+                <S.StatusFollowing>Following</S.StatusFollowing>
+              </S.Status>
+            )}
+          </S.RewardAmountContainer>
+        </S.TableGrid>
+      </S.ContributorContainer>
     ));
     return <S.FollowerCards>{_followers}</S.FollowerCards>;
   };
 
-  return <S.Container className={className}>{renderContent()}</S.Container>;
+  return (
+    <S.Container className={className}>
+      <S.Wrapper>
+        <S.Title>
+          Following — <span>10</span>
+        </S.Title>
+        <S.Search>
+          <Icon path={mdiMagnify} />
+          <input type="text" placeholder="Search" />
+        </S.Search>
+      </S.Wrapper>
+      {renderContent()}
+    </S.Container>
+  );
 };
 
 export default Following;
