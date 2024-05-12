67a5e11d5dbe3835b60076fbadbbb055c15aac30
Abdurrehman <52558124+MAbdurrehman1@users.noreply.github.com>
2024-05-13 01:29:36 +0500

    183 - Artwork Pagination (#338)
    * feat: artwork pagination on marketplace page and profile page
    
    * artwork list updated
    
    * Cleaning
    
    ---------
    
    Co-authored-by: bucky <buckyroberts@gmail.com>

--- src/pages/Art/Marketplace/index.tsx
+++ src/pages/Art/Marketplace/index.tsx
@@ -1,32 +1,46 @@
 import {useEffect, useMemo} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import orderBy from 'lodash/orderBy';
 
 import ArtworkCard from 'components/ArtworkCard';
 import EmptyText from 'components/EmptyText';
-import {getArtworks as _getArtworks} from 'dispatchers/artworks';
-import {getArtworks} from 'selectors/state';
+import InfiniteScroll from 'components/InfiniteScroll';
 import {AppDispatch, SFC} from 'types';
+import {getArtworks as _getArtworks, resetArtworks as _resetArtworks} from 'dispatchers/artworks';
+import {getArtworks as getArtworksState} from 'selectors/state';
 import * as S from './Styles';
 
 const Marketplace: SFC = ({className}) => {
-  const artworks = useSelector(getArtworks);
+  const artworksState = useSelector(getArtworksState);
+  const {artworks, hasMore, isLoading} = artworksState;
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     (async () => {
+      dispatch(_resetArtworks());
       await dispatch(_getArtworks({price_amount_isnull: false, price_core_isnull: false}));
     })();
   }, [dispatch]);
 
   const artworkList = useMemo(() => {
-    const _artworks = orderBy(Object.values(artworks), ['created_date'], ['desc']);
-    return _artworks.filter(({price_amount, price_core}) => !!price_amount && !!price_core);
+    return Object.values(artworks).filter(({price_amount, price_core}) => !!price_amount && !!price_core);
   }, [artworks]);
 
+  const fetchMoreArtworks = async () => {
+    if (!isLoading) {
+      await dispatch(_getArtworks());
+    }
+  };
+
   const renderArtworkCards = () => {
-    const _artworks = artworkList.map((artwork) => <ArtworkCard artwork={artwork} key={artwork.id} />);
-    return <S.ArtworkCards>{_artworks}</S.ArtworkCards>;
+    if (artworkList.length) {
+      return (
+        <S.ArtworkCards>
+          {artworkList.map((artwork) => (
+            <ArtworkCard artwork={artwork} key={artwork.id} />
+          ))}
+        </S.ArtworkCards>
+      );
+    }
   };
 
   const renderContent = () => {
@@ -44,8 +58,10 @@ const Marketplace: SFC = ({className}) => {
 
   return (
     <S.Container className={className}>
-      <S.Banner />
-      {renderContent()}
+      <InfiniteScroll dataLength={artworkList.length} hasMore={hasMore} next={fetchMoreArtworks} heightMargin={50}>
+        <S.Banner />
+        {renderContent()}
+      </InfiniteScroll>
     </S.Container>
   );
 };
