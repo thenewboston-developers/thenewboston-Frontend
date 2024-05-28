aab6ab8cebdf4886a36cc6f605d0a742e01ee852
Betsegaw Mesele [UGR-9758-14] <betsemese@gmail.com>
2024-05-28 20:06:36 +0300

    337 - Artworks for sale indicator(#263)

--- src/modals/ArtworkModal/Styles.ts
+++ src/modals/ArtworkModal/Styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 import UModal from 'components/Modal';
+import UTabs from 'components/Tabs';
 
 export const Modal = styled(UModal)`
   display: flex;
   flex-direction: column;
   width: 519px;
 `;
+export const Tabs = styled(UTabs)``;
--- src/modals/ArtworkModal/index.tsx
+++ src/modals/ArtworkModal/index.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction, useMemo} from 'react';
+import React, {Dispatch, SetStateAction, useMemo, useState} from 'react';
 import {useDispatch} from 'react-redux';
 import {Form, Formik} from 'formik';
 
@@ -11,6 +11,9 @@ import {AppDispatch, Artwork, SFC} from 'types';
 import {displayErrorToasts, displayToast} from 'utils/toasts';
 import yup from 'utils/yup';
 import * as S from './Styles';
+import Tab from 'components/Tab';
+import Icon from '@mdi/react';
+import {mdiCartArrowDown, mdiCartArrowUp} from '@mdi/js';
 
 export interface ArtworkModalProps {
   artwork?: Artwork;
@@ -30,8 +33,29 @@ const ArtworkModal: SFC<ArtworkModalProps> = ({
   currentIndex,
   setIsImageSaved,
 }) => {
+  enum SaveTypeTab {
+    SELL = 'Sell On Marketplace',
+    SAVE = 'Save As Draft',
+  }
+
   const dispatch = useDispatch<AppDispatch>();
   const priceCoreOptions = usePriceCoreOptions();
+  const [activeTab, setActiveTab] = useState<SaveTypeTab>(SaveTypeTab.SAVE);
+
+  const renderTabs = () => {
+    return (
+      <S.Tabs>
+        <Tab isActive={activeTab === SaveTypeTab.SELL} onClick={() => setActiveTab(SaveTypeTab.SELL)}>
+          <Icon path={mdiCartArrowDown} size={'16px'} />
+          Sell on Marketplace
+        </Tab>
+        <Tab isActive={activeTab === SaveTypeTab.SAVE} onClick={() => setActiveTab(SaveTypeTab.SAVE)}>
+          <Icon path={mdiCartArrowUp} size={'16px'} />
+          Save As Draft
+        </Tab>
+      </S.Tabs>
+    );
+  };
 
   const initialValues = useMemo(
     () => ({
@@ -85,20 +109,33 @@ const ArtworkModal: SFC<ArtworkModalProps> = ({
     <S.Modal className={className} close={close} header={artwork ? 'Edit Artwork' : 'Create Artwork'}>
       <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
         {({dirty, errors, isSubmitting, isValid, touched}) => (
-          <Form>
-            <Input errors={errors} label="Name" name="name" touched={touched} />
-            <Input errors={errors} label="Description" name="description" touched={touched} />
-            <Select errors={errors} label="Currency" name="price_core" options={priceCoreOptions} touched={touched} />
-            <Input errors={errors} label="Price Amount" name="price_amount" touched={touched} type="number" />
-            <Button
-              dirty={dirty}
-              disabled={isSubmitting}
-              isSubmitting={isSubmitting}
-              isValid={isValid}
-              text="Submit"
-              type={ButtonType.submit}
-            />
-          </Form>
+          <>
+            {renderTabs()}
+            <Form>
+              <Input errors={errors} label="Name" name="name" touched={touched} />
+              <Input errors={errors} label="Description" name="description" touched={touched} />
+              {activeTab === SaveTypeTab.SELL && (
+                <>
+                  <Select
+                    errors={errors}
+                    label="Currency"
+                    name="price_core"
+                    options={priceCoreOptions}
+                    touched={touched}
+                  />
+                  <Input errors={errors} label="Price Amount" name="price_amount" touched={touched} type="number" />
+                </>
+              )}{' '}
+              <Button
+                dirty={dirty}
+                disabled={isSubmitting}
+                isSubmitting={isSubmitting}
+                isValid={isValid}
+                text={activeTab === SaveTypeTab.SELL ? 'Sell on Marketplace' : 'Save as Draft'}
+                type={ButtonType.submit}
+              />
+            </Form>
+          </>
         )}
       </Formik>
     </S.Modal>
