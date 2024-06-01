e08c34b9a37c19f1bc9a0b29c3cf420ab4611954
Betsegaw Mesele <146959506+bexeGrace@users.noreply.github.com>
2024-06-01 03:10:41 +0300

    292 maximize post image onclick (#381)
    * 335 - Maximize post image onclick
    
    * 336 - Maximize Post Image onclick(#292)
    
    * created image modal
    
    * Done edits from the preview
    
    * FIxed reviewed issues
    
    * cleaning
    
    ---------
    
    Co-authored-by: Abdurrehman <abdurrehman.user.95@gmail.com>

---
+++ src/modals/FullScreenImageModal/index.tsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+import {createPortal} from 'react-dom';
+
+import {SFC} from 'types';
+
+import * as S from './Styles';
+
+export interface ImageModalProps {
+  close(): void;
+  imageSrc: string;
+}
+
+const FullScreenImageModal: SFC<ImageModalProps> = ({imageSrc, close}) => {
+  const [preview, setPreview] = useState<string | null>(imageSrc);
+
+  return createPortal(
+    <>
+      <S.Modal>
+        <S.ImagePreviewWrapper>
+          <S.ImagePreviewCustom
+            onClear={() => {
+              setPreview(null);
+              close();
+            }}
+            src={preview}
+          ></S.ImagePreviewCustom>
+        </S.ImagePreviewWrapper>
+      </S.Modal>
+    </>,
+    document.getElementById('modal-root') as HTMLElement,
+  );
+};
+
+export default FullScreenImageModal;
