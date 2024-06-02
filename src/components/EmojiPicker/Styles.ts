72a01484c559003436f5326e2c3e6959b3c47b5d
kiranmistary0697 <77836546+kiranmistary0697@users.noreply.github.com>
2024-06-02 21:06:09 +0530

    368 - Add emoji selector to create post and edit comment forms (#385)
    * 368 - Add emoji selector to create post and edit comment forms
    
    * Minor changes on EmojiPicker
    
    * cleaning...
    
    ---------
    
    Co-authored-by: Abdurrehman <abdurrehman.user.95@gmail.com>

---
+++ src/components/EmojiPicker/Styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import {colors} from 'styles';
+
+export const EmojiBox = styled.div`
+  position: absolute;
+  right: 0;
+  top: 85px;
+  z-index: 1;
+`;
+
+export const EmojiButton = styled.button<{$isOpenEmojiBox?: boolean | false}>`
+  background: none;
+  border: none;
+  color: ${({$isOpenEmojiBox}) => ($isOpenEmojiBox ? '#5a80ab' : colors.gray)};
+  cursor: pointer;
+  font-weight: 600;
+  outline: none;
+  position: absolute;
+  right: 15px;
+  top: 34px;
+`;
