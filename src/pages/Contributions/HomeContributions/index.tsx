6f4abaf8d630e5fa8bc5c67fe1255919e99c9ea5
Abdurrehman <abdurrehman.user.95@gmail.com>
2024-07-08 18:48:31 +0500

    working on contributions skeletons - top contributors skeleton done

--- src/pages/Contributions/HomeContributions/index.tsx
+++ src/pages/Contributions/HomeContributions/index.tsx
@@ -1,5 +1,7 @@
 import ContributionsCumulativeChart from 'components/Contributions/ContributionsCumulativeChart';
 import ContributionsList from 'components/Contributions/Contributions';
+import ContributionsSkeleton from 'components/Contributions/Skeleton';
+import TopContributorsSkeleton from 'components/Contributions/Skeleton/TopContributorsSkeleton';
 import Loader from 'components/Loader';
 import TopContributors from 'components/Contributions/TopContributors';
 import useContributions from 'hooks/useContributions';
@@ -9,7 +11,19 @@ const HomeContributions = () => {
   const {items, isInitialLoading} = useContributions();
   const contributionsList = Object.values(items);
 
+  const getSkeleton = (n: number) => <ContributionsSkeleton dataLength={n} />;
+
+  return (
+    <Row>
+      <Col size={4}>
+        <TopContributorsSkeleton />
+      </Col>
+    </Row>
+  );
+
   if (isInitialLoading) {
+    // return getSkeleton(4);
+    return <TopContributorsSkeleton />;
     return <Loader className="align-screen-center" size={24} />;
   }
 
