de1b114fe4787b1101485c4929040933fab1ad6d
Muhammad Abdurrehman <abdurrehman.user.95@gmail.com>
2024-09-04 16:38:06 +0500

    feat: post comment notification to owner

--- src/pages/Notifications/Notification/index.tsx
+++ src/pages/Notifications/Notification/index.tsx
@@ -10,6 +10,7 @@ import * as S from './Styles';
 
 enum NotificationType {
   ARTWORK_PURCHASE = 'ARTWORK_PURCHASE',
+  POST_COMMENT = 'POST_COMMENT',
 }
 
 export interface NotificationProps {
@@ -43,9 +44,30 @@ const Notification: SFC<NotificationProps> = ({className, notification}) => {
     );
   };
 
+  const renderPostCommentNotification = () => {
+    return (
+      <S.ArtworkPurchaseNotificationContainer>
+        <Link to={`/profile/${notification.payload.commenter.id}`}>
+          <Avatar src={notification.payload.commenter.avatar} size="45px" />
+          <S.IconBrush path={mdiBrushOutline} size="23px" />
+        </Link>
+        <S.TextContainer>
+          <S.Link to={`/profile/${notification.payload.commenter.id}`}>
+            {notification.payload.commenter.username}
+          </S.Link>{' '}
+          commented on your <strong>Post</strong>:"{notification.payload.comment}"
+          <br />
+          <S.TimeStamp>{longDate(notification.created_date)}</S.TimeStamp>
+        </S.TextContainer>
+        {renderRedDot()}
+      </S.ArtworkPurchaseNotificationContainer>
+    );
+  };
+
   const renderContent = () => {
     const notificationTypes: {[key in NotificationType]: () => ReactNode} = {
       [NotificationType.ARTWORK_PURCHASE]: renderArtworkPurchaseNotification,
+      [NotificationType.POST_COMMENT]: renderPostCommentNotification,
     };
 
     const renderFunction = notificationTypes[notification.payload.notification_type as NotificationType];
