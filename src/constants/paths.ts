ee008912a985d71d16a87f1a9328bd0070e68c64
Afnaaan <user@users-MacBook-Pro.local>
2025-05-16 19:30:20 +0530

    removed modules: art, contribution, shop, Uni versity

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -6,21 +6,11 @@ export const PATH_AUTHENTICATION = {
   LOGOUT: '/logout',
   SIGNUP: '/createAccount',
 };
-export const BASE_CONTRIBUTIONS = '/contributions';
-export const PATH_CONTRIBUTIONS = {
-  HOME: `${BASE_CONTRIBUTIONS}/home`,
-  LEARN_MORE: `${BASE_CONTRIBUTIONS}/learn-more`,
-  ROOT: `${BASE_CONTRIBUTIONS}/${WILDCARD}`,
-  SELF: `${BASE_CONTRIBUTIONS}/self`,
-};
-export const PATH_COURSES = '/university/courses';
-export const PATH_COURSES_SELF = '/university/courses/self';
+
 export const PATH_CURRENCIES = `/currencies/${WILDCARD}`;
 export const PATH_EXCHANGE = `/exchange/${WILDCARD}`;
 export const PATH_FEED = '/feed';
 export const PATH_IA = '/ia/:id?';
-export const PATH_LECTURES = '/university/lectures';
-export const PATH_LECTURES_SELF = '/university/lectures/self';
 export const PATH_NOTIFICATIONS = '/notifications';
 export const PATH_PROFILE = `/profile/:id/${WILDCARD}`;
 export const PATH_SHOP = `/shop/${WILDCARD}`;
--- src/pages/Contributions/Layout/Styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div``;
-
-export const OutletContainer = styled.div`
-  height: 100%;
-  overflow-y: auto;
-  padding: 32px;
-`;
