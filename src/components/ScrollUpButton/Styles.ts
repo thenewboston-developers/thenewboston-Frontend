b4bc69e87d092bf82a0dd2e2498213b0c6b77615
Bucky Roberts <buckyroberts@gmail.com>
2025-05-26 22:18:39 -0400

    Random styling updates (#479)
    * Fixes Bottom of posts get cut off
    
    * Fixes Only show one currency in buy/sell dropdown
    
    * Remove icon and update empty state message

--- src/components/ScrollUpButton/Styles.ts
+++ src/components/ScrollUpButton/Styles.ts
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  position: fixed;
-  bottom: 50px;
-  right: 30px;
   background-color: #333;
-  color: #fff;
-  border: none;
   border-radius: 50%;
-  padding: 10px;
-  cursor: pointer;
-  z-index: 1000;
+  border: none;
+  bottom: 24px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  color: #fff;
+  cursor: pointer;
+  padding: 10px;
+  position: fixed;
+  right: 24px;
+  z-index: 1000;
 `;
