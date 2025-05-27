66086bfb62d8db7b4cc01f6049264c67bf471d4c
bucky <buckyroberts@gmail.com>
2025-05-26 22:09:19 -0400

    Fixes Bottom of posts get cut off

--- src/pages/Feed/Styles.ts
+++ src/pages/Feed/Styles.ts
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100%;
   overflow-y: auto;
-  padding: 24px 16px;
+  padding: 0 16px;
 `;
 
 export const PostContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
-  margin: 0 auto;
+  margin: 24px auto;
   max-width: 720px;
 `;
