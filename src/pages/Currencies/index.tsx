158352aac44ed1d9c0d0554e9f7eab26f090f2aa
bucky <buckyroberts@gmail.com>
2025-05-28 18:48:26 -0400

    489 - Design updates

--- src/pages/Currencies/index.tsx
+++ src/pages/Currencies/index.tsx
@@ -13,7 +13,6 @@ const Currencies: SFC = ({className}) => {
     <S.Container className={className}>
       <Routes>
         <Route element={<Layout />}>
-          {/* TODO: replace these hardcoded paths with those in constants/paths.ts */}
           <Route path="*" element={<Navigate to="/currencies/home" replace />} />
           <Route path="/home" element={<Home />} />
           <Route path="/learn-more" element={<LearnMore />} />
