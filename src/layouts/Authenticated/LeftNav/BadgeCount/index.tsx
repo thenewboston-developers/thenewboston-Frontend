259c5046abe222ecaddd6d1dd9359c97194c76a4
bucky <buckyroberts@gmail.com>
2025-05-28 23:49:27 -0400

    Update badges

--- src/layouts/Authenticated/LeftNav/BadgeCount/index.tsx
+++ src/layouts/Authenticated/LeftNav/BadgeCount/index.tsx
@@ -1,24 +1,22 @@
 import {useMemo} from 'react';
 
-import {BadgeStyle} from 'components/Badge';
 import {SFC} from 'types';
 
 import * as S from './Styles';
 
 interface BadgeCountProps<T> {
-  badgeStyle?: BadgeStyle;
   countFunction?: (items: T[]) => number;
   items: T[];
 }
 
-const BadgeCount: SFC<BadgeCountProps<any>> = ({badgeStyle = BadgeStyle.danger, className, countFunction, items}) => {
+const BadgeCount: SFC<BadgeCountProps<any>> = ({className, countFunction, items}) => {
   const computedCount = useMemo(() => {
     return countFunction ? countFunction(items) : items.length;
   }, [items, countFunction]);
 
   if (computedCount <= 0) return null;
 
-  return <S.BadgeCount badgeStyle={badgeStyle} children={computedCount.toString()} className={className} />;
+  return <S.BadgeCount className={className}>{computedCount}</S.BadgeCount>;
 };
 
 export default BadgeCount;
