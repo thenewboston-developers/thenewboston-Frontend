b08a04a1b48c62ba126dab76b380f0ff66b185cf
bucky <buckyroberts@gmail.com>
2025-05-31 01:22:32 -0400

    More styling updates

--- src/modals/CommentEditModal/Styles.ts
+++ src/modals/CommentEditModal/Styles.ts
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
 
+import {Textarea as UTextarea} from 'components/FormElements';
 import UModal from 'components/Modal';
+
+export const Div = styled.div`
+  position: relative;
+`;
 
 export const Modal = styled(UModal)`
   display: flex;
   flex-direction: column;
   width: 360px;
+`;
+
+export const Textarea = styled(UTextarea)`
+  max-height: 350px;
+  overflow-y: auto;
+  padding-right: 43px;
 `;
