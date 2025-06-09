a22ded1a4d1ba9d2e5672f3cf6d5c10f5b71d9d9
bucky <buckyroberts@gmail.com>
2025-06-08 20:55:30 -0400

    More cleaning

--- src/modals/CurrencyModal/Styles.ts
+++ src/modals/CurrencyModal/Styles.ts
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-import {Input as _Input, Textarea as _Textarea} from 'components/FormElements';
+import {Input as UInput, Textarea as UTextarea} from 'components/FormElements';
 import UModal from 'components/Modal';
 
-export const Input = styled(_Input)`
+export const Input = styled(UInput)`
   margin-bottom: 24px;
   width: 360px;
 `;
 
 export const Modal = styled(UModal)``;
 
-export const Textarea = styled(_Textarea)`
+export const Textarea = styled(UTextarea)`
   margin-bottom: 24px;
   width: 360px;
 `;
