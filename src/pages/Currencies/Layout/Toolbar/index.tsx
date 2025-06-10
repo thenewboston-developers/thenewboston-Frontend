b655a599f78b59313a331db8e3aba52c838ac258
bucky <buckyroberts@gmail.com>
2025-06-09 23:59:07 -0400

    Toolbar menu item styling

--- src/pages/Currencies/Layout/Toolbar/index.tsx
+++ src/pages/Currencies/Layout/Toolbar/index.tsx
@@ -9,18 +9,14 @@ import * as S from './Styles';
 const Toolbar: SFC = ({className}) => {
   const [currencyModalIsOpen, toggleCurrencyModal] = useToggle(false);
 
-  const renderButton = () => {
-    return <Button onClick={toggleCurrencyModal} text="Create Currency" />;
-  };
-
   return (
     <>
       <S.Container className={className}>
-        <S.Center>
+        <S.MenuItems>
           <ToolbarMenuLink text="Home" to="/currencies/home" />
           <ToolbarMenuLink text="Learn More" to="/currencies/learn-more" />
-        </S.Center>
-        <S.ButtonContainer>{renderButton()}</S.ButtonContainer>
+        </S.MenuItems>
+        <Button onClick={toggleCurrencyModal} text="Create Currency" />
       </S.Container>
       {currencyModalIsOpen ? <CurrencyModal close={toggleCurrencyModal} /> : null}
     </>
--- src/pages/Exchange/Toolbar/Styles.ts
+++ src/pages/Exchange/Toolbar/Styles.ts
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 import {toolbarStyle} from 'styles';
 
-export const Center = styled.div`
-  align-items: center;
-  display: flex;
-`;
-
 export const Container = styled.div`
   ${toolbarStyle};
 `;
 
 export const Left = styled.div``;
+
+export const MenuItems = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 8px;
+`;
 
 export const Right = styled.div``;
