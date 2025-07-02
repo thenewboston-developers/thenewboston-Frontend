36df395b6f178fb451497c8d03f2955e0992ac46
bucky <buckyroberts@gmail.com>
2025-07-01 20:23:09 -0400

    Better animation

--- src/components/FullScreenImage/Styles.ts
+++ src/components/FullScreenImage/Styles.ts
@@ -1,7 +1,28 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    transform: scale(0.95);
+    opacity: 0;
+  }
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+`;
 
 export const Container = styled.div`
   align-items: center;
+  animation: ${fadeIn} 0.2s ease-out;
   cursor: pointer;
   display: flex;
   height: 100%;
@@ -10,6 +31,7 @@ export const Container = styled.div`
 `;
 
 export const Img = styled.img`
+  animation: ${scaleIn} 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94);
   cursor: default;
   max-height: 90vh;
   max-width: 90vw;
