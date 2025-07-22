8d724f838992533658533ebfe03ec5ba0c34b626
bucky <buckyroberts@gmail.com>
2025-07-22 19:58:53 -0400

    Selector getCurrenciesOrdered warning

--- src/selectors/state.ts
+++ src/selectors/state.ts
@@ -1,13 +1,15 @@
+import {createSelector} from '@reduxjs/toolkit';
+
 import {RootState} from 'types';
 
 export const getAssetPairs = (state: RootState) => state.assetPairs;
 export const getAuthentication = (state: RootState) => state.authentication;
 export const getComments = (state: RootState) => state.comments;
 export const getCurrencies = (state: RootState) => state.currencies.byId;
-export const getCurrenciesOrdered = (state: RootState) => {
-  const {byId, ids} = state.currencies;
-  return ids.map((id) => byId[id]);
-};
+export const getCurrenciesOrdered = createSelector(
+  [(state: RootState) => state.currencies.byId, (state: RootState) => state.currencies.ids],
+  (byId, ids) => ids.map((id) => byId[id]),
+);
 export const getExchangeOrders = (state: RootState) => state.exchangeOrders.exchangeOrders;
 export const hasMoreExchangeOrders = (state: RootState) => state.exchangeOrders.hasMore;
 export const isLoadingExchangeOrders = (state: RootState) => state.exchangeOrders.isLoading;
