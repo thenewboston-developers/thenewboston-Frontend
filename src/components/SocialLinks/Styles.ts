9f5809b40408a47747d36ce2bc3e1c922fd435e4
bucky <buckyroberts@gmail.com>
2025-07-23 20:32:17 -0400

    Styling update

--- src/components/SocialLinks/Styles.ts
+++ src/components/SocialLinks/Styles.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-import {colors} from 'styles';
-
 export const Container = styled.div`
   align-items: center;
   display: flex;
@@ -13,15 +11,12 @@ export const Container = styled.div`
 export const SocialLink = styled.a`
   align-items: center;
   border-radius: 50%;
-  color: ${colors.palette.gray[600]};
   display: flex;
   height: 36px;
   justify-content: center;
   transition: all 0.2s ease;
 
   &:hover {
-    background-color: ${colors.palette.gray[100]};
-    color: ${colors.palette.gray[900]};
     transform: translateY(-2px);
   }
 `;
