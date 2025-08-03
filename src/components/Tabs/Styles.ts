100260224a4690ee3559a4daa3785267bf8d851c
Bucky Roberts <buckyroberts@gmail.com>
2025-08-03 15:45:05 -0400

    809 - Tabs responsiveness (#812)
    * 809 - Tabs responsiveness
    
    * Cleaning
    
    * Stack on mobile
    
    * Final cleaning

--- src/components/Tabs/Styles.ts
+++ src/components/Tabs/Styles.ts
@@ -1,17 +1,27 @@
 import styled from 'styled-components';
 
-import {colors} from 'styles';
+import {breakpoints, colors} from 'styles';
 
 const HEIGHT = 48;
 const BORDER_RADIUS = HEIGHT / 2;
 
-export const Container = styled.div`
+export const Container = styled.div<{$stackOnMobile: boolean}>`
   background: ${colors.palette.gray[200]};
-  border-radius: ${`${BORDER_RADIUS}px`};
   border: 1px solid #dfdfdf;
+  border-radius: ${`${BORDER_RADIUS}px`};
   display: flex;
   height: ${`${HEIGHT}px`};
   overflow-x: auto;
   padding: 6px;
   width: fit-content;
+
+  ${({$stackOnMobile}) =>
+    $stackOnMobile &&
+    `
+    @media (max-width: ${breakpoints.mobile}) {
+      flex-direction: column;
+      height: auto;
+      width: 100%;
+    }
+  `}
 `;
