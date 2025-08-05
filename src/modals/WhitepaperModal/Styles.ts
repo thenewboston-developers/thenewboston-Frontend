2f6a4688a57d66e5e131c7d9b1b498ff5304afd7
bucky <buckyroberts@gmail.com>
2025-08-04 21:14:35 -0400

    813 - Edit Whitepaper modal responsiveness

--- src/modals/WhitepaperModal/Styles.ts
+++ src/modals/WhitepaperModal/Styles.ts
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 
 import {Textarea as UTextarea} from 'components/FormElements';
 import UModal from 'components/Modal';
-import {colors, markdownStyle} from 'styles';
+import {breakpoints, colors, markdownStyle} from 'styles';
 
 export const Modal = styled(UModal)`
   width: 800px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    width: 90%;
+  }
+
+  @media (max-width: ${breakpoints.mobile}) {
+    width: 95%;
+  }
 `;
 
 export const PreviewContainer = styled.div`
@@ -16,6 +24,15 @@ export const PreviewContainer = styled.div`
   height: 480px;
   overflow-y: auto;
   padding: 16px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    height: 400px;
+  }
+
+  @media (max-width: ${breakpoints.mobile}) {
+    height: 300px;
+    padding: 12px;
+  }
 `;
 
 export const TabsContainer = styled.div`
@@ -26,4 +43,14 @@ export const Textarea = styled(UTextarea)`
   height: 480px;
   min-height: 480px;
   width: 100%;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    height: 400px;
+    min-height: 400px;
+  }
+
+  @media (max-width: ${breakpoints.mobile}) {
+    height: 300px;
+    min-height: 300px;
+  }
 `;
