a66f0552c1ec7b1ab2cbc9270dde4dc638e84f7b
bucky <buckyroberts@gmail.com>
2025-08-07 20:45:08 -0400

    Cleaning

--- src/api/tradeHistoryItems.ts
+++ src/api/tradeHistoryItems.ts
@@ -4,10 +4,9 @@ import {TradeHistoryItemsResponse} from 'types';
 
 const BASE_URL = `${process.env.REACT_APP_API_URL}/api/trade-history-items`;
 
-export const getTradeHistoryItems = async (ordering?: string): Promise<TradeHistoryItemsResponse> => {
+export const getTradeHistoryItems = async (ordering: string): Promise<TradeHistoryItemsResponse> => {
   try {
-    const params = ordering ? {ordering} : undefined;
-    const response = await axios.get<TradeHistoryItemsResponse>(BASE_URL, {params});
+    const response = await axios.get<TradeHistoryItemsResponse>(BASE_URL, {params: {ordering}});
     return response.data;
   } catch (error) {
     throw error;
