0ab5973ac69803dcf153f920c68719fd2e25cdc6
Bucky Roberts <buckyroberts@gmail.com>
2025-08-10 22:45:30 -0400

    844 - Edit Profile modal responsiveness (#848)

--- src/modals/ProfileEditModal/Styles.ts
+++ src/modals/ProfileEditModal/Styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import ImagePreview from 'components/ImagePreview';
 import UModal, {ModalBody as UModalBody} from 'components/Modal';
-import {colors} from 'styles';
+import {breakpoints, colors} from 'styles';
 
 export const AvatarPreview = styled(ImagePreview)``;
 
@@ -10,6 +10,10 @@ export const BannerPreview = styled(ImagePreview)`
   img {
     max-height: 225px;
     max-width: 400px;
+
+    @media (max-width: ${breakpoints.mobile}) {
+      max-width: 100%;
+    }
   }
 `;
 
@@ -20,12 +24,16 @@ export const ErrorMessage = styled.div`
 `;
 
 export const Modal = styled(UModal)`
-  max-height: 90vh;
+  max-height: 80vh;
   width: 680px;
+
+  @media (max-width: ${breakpoints.mobile}) {
+    width: 95%;
+  }
 `;
 
 export const ModalBody = styled(UModalBody)`
-  max-height: calc(90vh - 200px);
+  max-height: calc(80vh - 200px);
   overflow-y: auto;
 `;
 
@@ -34,6 +42,11 @@ export const Section = styled.div`
     border-top: 1px solid ${colors.palette.gray[200]};
     margin-top: 24px;
     padding-top: 24px;
+
+    @media (max-width: ${breakpoints.mobile}) {
+      margin-top: 20px;
+      padding-top: 20px;
+    }
   }
 `;
 
@@ -42,10 +55,23 @@ export const SectionHeading = styled.h3`
   font-size: 16px;
   font-weight: 600;
   margin: 0 0 16px;
+
+  @media (max-width: ${breakpoints.mobile}) {
+    font-size: 14px;
+    margin: 0 0 12px;
+  }
 `;
 
 export const SocialMediaGrid = styled.div`
   display: grid;
   gap: 0 24px;
   grid-template-columns: 1fr 1fr;
+
+  @media (max-width: ${breakpoints.mobile}) {
+    gap: 0 16px;
+  }
+
+  @media (max-width: ${breakpoints.mini}) {
+    grid-template-columns: 1fr;
+  }
 `;
