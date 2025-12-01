429d9aafec5b6fd672fe1ed6fb67a2ff9e069c92
Bucky Roberts <buckyroberts@gmail.com>
2025-11-30 19:15:28 -0500

    893 - Asset pair updates (#894)
    * 893 - Asset pair updates
    
    * Removing markdown file

--- src/api/assetPairs.ts
+++ src/api/assetPairs.ts
@@ -5,6 +5,17 @@ import {authorizationHeaders} from 'utils/authentication';
 
 const BASE_URL = `${process.env.REACT_APP_API_URL}/api/asset-pairs`;
 
+export const getAssetPair = async (assetPairId: number | string): Promise<AssetPair> => {
+  try {
+    const response = await axios.get<AssetPair>(`${BASE_URL}/${assetPairId}`, {
+      ...authorizationHeaders(),
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const getAssetPairs = async (params?: {
   primary_currency_ticker?: string;
   secondary_currency_ticker?: string;
--- src/dispatchers/assetPairs.ts
+++ src/dispatchers/assetPairs.ts
@@ -1,6 +1,12 @@
-import {getAssetPairs as _getAssetPairs} from 'api/assetPairs';
+import {getAssetPair as _getAssetPair, getAssetPairs as _getAssetPairs} from 'api/assetPairs';
 import {setAssetPairs} from 'store/assetPairs';
 import {AppDispatch} from 'types';
+
+export const getAssetPair = (assetPairId: number | string) => async (dispatch: AppDispatch) => {
+  const responseData = await _getAssetPair(assetPairId);
+  dispatch(setAssetPairs([responseData]));
+  return responseData;
+};
 
 export const getAssetPairs = (params?: {page?: number; page_size?: number}) => async (dispatch: AppDispatch) => {
   const responseData = await _getAssetPairs(params);
