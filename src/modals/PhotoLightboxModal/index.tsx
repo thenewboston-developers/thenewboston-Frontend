7cabdf6838ffed615dc53e516bc9da36fc2532e0
bucky <buckyroberts@gmail.com>
2025-12-30 20:07:58 -0500

    Proper delete logic

--- src/modals/PhotoLightboxModal/index.tsx
+++ src/modals/PhotoLightboxModal/index.tsx
@@ -1,6 +1,7 @@
-import {MouseEvent, useEffect} from 'react';
+import {MouseEvent, useEffect, useRef} from 'react';
 import {createPortal} from 'react-dom';
 import {useSelector} from 'react-redux';
+import {useNavigate, useParams} from 'react-router-dom';
 import {mdiClose} from '@mdi/js';
 
 import Icon from 'components/Icon';
@@ -16,9 +17,34 @@ export interface PhotoLightboxModalProps {
 }
 
 const PhotoLightboxModal: SFC<PhotoLightboxModalProps> = ({className, close, post}) => {
+  const hasSeenPostRef = useRef(false);
+  const {id} = useParams();
   const isMobile = useIsMobile();
+  const navigate = useNavigate();
   const posts = useSelector(getPosts);
-  const activePost = posts.find((storedPost) => storedPost.id === post.id) || post;
+  const profileId = id || `${post.owner.id}`;
+  const storedPost = posts.find((postData) => postData.id === post.id);
+  const activePost = storedPost || post;
+
+  useEffect(() => {
+    if (storedPost) {
+      hasSeenPostRef.current = true;
+    }
+  }, [storedPost]);
+
+  useEffect(() => {
+    if (storedPost) {
+      if (!storedPost.image) {
+        close();
+      }
+      return;
+    }
+
+    if (hasSeenPostRef.current) {
+      close();
+      navigate(`/profile/${profileId}/photos`);
+    }
+  }, [close, navigate, profileId, storedPost]);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
