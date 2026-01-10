dc7041b6a2b7944866de7bd3522fcfab1013a056
bucky <buckyroberts@gmail.com>
2026-01-10 13:05:20 -0500

    Another chapter and image update

--- src/pages/Bonsai/LearnMore/chapters.ts
+++ src/pages/Bonsai/LearnMore/chapters.ts
@@ -1,21 +1,27 @@
-import BonsaiLearnMoreChapter1 from './Chapter01';
-import BonsaiLearnMoreChapter2 from './Chapter02';
-import BonsaiLearnMoreChapter3 from './Chapter03';
+import BonsaiLearnMoreChapter01 from './Chapter01';
+import BonsaiLearnMoreChapter02 from './Chapter02';
+import BonsaiLearnMoreChapter03 from './Chapter03';
+import BonsaiLearnMoreChapter04 from './Chapter04';
 
 export const bonsaiLearnMoreChapters = [
   {
-    Component: BonsaiLearnMoreChapter1,
+    Component: BonsaiLearnMoreChapter01,
     path: 'chapter-1',
     title: 'Chapter 1: The Seed',
   },
   {
-    Component: BonsaiLearnMoreChapter2,
+    Component: BonsaiLearnMoreChapter02,
     path: 'chapter-2',
     title: 'Chapter 2: Germination and the First Year',
   },
   {
-    Component: BonsaiLearnMoreChapter3,
+    Component: BonsaiLearnMoreChapter03,
     path: 'chapter-3',
     title: 'Chapter 3: Roots',
+  },
+  {
+    Component: BonsaiLearnMoreChapter04,
+    path: 'chapter-4',
+    title: 'Chapter 4: Trunk',
   },
 ];
